f3cf1cb89baeb0026d3f13c63bb10d6f333df7d4
Acosta Gustavo <gustavo080487@gmail.com>
2023-11-20 10:13:44 -0300

    entregable .net

--- entregableNET/Controllers/TasksController.cs
+++ entregableNET/Controllers/TasksController.cs
@@ -5,7 +5,32 @@ namespace entregableNET.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    
     public class TasksController : ControllerBase
     {
+        [HttpGet]
+        public ActionResult GETall()
+        {
+            return Ok();
+        }
+
+        [HttpPost]  
+        public ActionResult POST(Task task)
+        {
+            return Ok(task);
+        }
+
+        [HttpPut]
+        public ActionResult PUT(Task task)
+        {
+            return Ok(task);
+        }
+
+        [HttpDelete]
+        public ActionResult DELETE(Task task)
+        {
+            return Ok(task);
+
+        }
     }
 }
