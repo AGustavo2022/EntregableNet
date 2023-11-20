77b7d8f477eea2dc90db84d67d3765d9e0ddad79
Acosta Gustavo <gustavo080487@gmail.com>
2023-11-20 15:54:50 -0300

    entregableNET

---
+++ entregableNET/DataAccess/ContextDB.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using entregableNET.Entities;
+
+namespace entregableNET.DataAccess
+{
+    public class ContextDB : DbContext
+    {
+        public ContextDB(DbContextOptions options) : base(options)
+        { 
+
+        }
+
+        public DbSet<Tasks> Tasks { get; set; }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
+        
+
+
---
+++ entregableNET/Entities/Tasks.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace entregableNET.Entities
+{
+    [Table("Tasks")]
+    public class Tasks
+    {
+        [Key]
+        [Column("tasks_id")]
+        public int Id { get; set; }
+
+        [Column("title", TypeName = "VARCHAR(50)")]
+        public string Title { get; set; }
+
+        [Column("description", TypeName = "VARCHAR(200)")]
+        public string Description { get; set; }
+
+        [Column("completed")]
+        public bool IsCompleted { get; set; }
+    }
+}
+
+
+
